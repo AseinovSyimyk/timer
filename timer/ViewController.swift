5e92fbc891ca22daadfb9aea0a5851f5aab2e704
AseinovSyimyk <aseinovsyimyk52@gmail.com>
2023-12-12 21:07:57 +0600

    add

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    var timer: Timer?
+    var seconds = 0
+    var isTimerRunning = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateTimerLabel()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-
-
+    
+    
+    @objc func updateTimer() {
+        if isTimerRunning {
+            seconds += 1
+            updateTimerLabel()
+        }
+    }
+    
+    func updateTimerLabel() {
+        let minutes = seconds / 60
+        let remainingSeconds = seconds % 60
+        timerLabel.text = String(format: "%02d:%02d", minutes, remainingSeconds)
+    }
+    
+    @IBAction func pauseButton(_ sender: UIButton) {
+        isTimerRunning = false
+        pauseButton.isEnabled = false
+        playButton.isEnabled = true
+    }
+    
+    @IBAction func playButton(_ sender: UIButton) {
+        isTimerRunning = true
+        pauseButton.isEnabled = true
+        playButton.isEnabled = false
+    }
 }
 
